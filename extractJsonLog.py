3c67a8b1b3c5ef994a07738aa54a30e1f89a26be
icecoins <woyaogtmdttkp@gmail.com>
2025-10-23 23:18:39 +0400

    Update args parsing

--- extractJsonLog.py
+++ extractJsonLog.py
@@ -1,25 +1,54 @@
 import json
 import matplotlib.pyplot as plt
+import argparse
+import os
 
-daily_values = []
-total_values = []
-fileName = f'./2025-10-01_20:00:00-2025-10-01_20:00:00.json'
-with open(fileName, 'r') as f:
-    for l in f.readlines():
-        t = l.replace('\n','')[:-1]
-        js = json.loads(t)
-        daily_values.append(float(js['TODAY_ROI'])*100)
-        total_values.append(float(js['TOTAL_ROI'])*100)
+def setupArgParser():
+    parser.add_argument("-logpath", type=str, default="", help="")
+    return parser
 
-x = list(range(1, len(daily_values) + 1))
+def draw(log_dir, output_dir):
+    os.makedirs(os.path.dirname(output), exist_ok=True)
+    daily_values = []
+    total_values = []
+    with open(log_dir, 'r', encoding="utf-8") as f:
+        for l in f.readlines():
+            t = l.replace('\n','')
+            if(t.endswith(',')):
+                t = t[:-1]
+            try:
+                js = json.loads(t)
+            except:
+                continue
+            daily_values.append(float(js['TODAY_ROI'])*100)
+            total_values.append(float(js['TOTAL_ROI'])*100)
 
-plt.figure(figsize=(10, 6))
-plt.plot(x, daily_values, marker='o', color='blue', label='StepReturn')
-plt.plot(x, total_values, marker='s', color='red', label='TotalReturn')
+    x = list(range(1, len(daily_values) + 1))
 
-plt.title('Step and Total Return Line Chart')
-plt.xlabel('Date')
-plt.ylabel('Return(%)')
-plt.legend()
-plt.grid(True)
-plt.show()
+    plt.figure(figsize=(10, 6))
+    plt.plot(x, daily_values, marker='o', color='blue', label='StepReturn')
+    plt.plot(x, total_values, marker='s', color='red', label='TotalReturn')
+
+    plt.title('Step and Total Return Line Chart')
+    plt.xlabel('Date')
+    plt.ylabel('Return(%)')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(output_dir)
+    #plt.show()
+    plt.close()
+
+def checkArgs():
+    if(not args.logpath or args.logpath == ""):
+        print(f'logpath is empty')
+        return False
+    return True
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args, unknown = setupArgParser().parse_known_args()
+    output = str(args.logpath).replace('.json', '.png')
+    print(f'output: {output}')
+    if(checkArgs()):
+        draw(log_dir=args.logpath, output_dir=output)
+    
